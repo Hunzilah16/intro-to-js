79deff22d9962139e37bfc82d971e80bfcf94077
Hunzilah16 <hunzilah16@gmail.com>
2024-03-23 21:06:08 +0500

    prompt alirt some algebra and some nifty tricks

---
+++ Script snippet #1.js
@@ -0,0 +1,11 @@
+var have = prompt("write somthing");
+
+var charecters = have.length;
+
+var remaining =250-charecters ;
+
+alert( "you have used " + charecters +   " and have "   +remaining +  " left");
+
+
+
+
