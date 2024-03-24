44e8c484e5133c52f4ffe63a8bff8425bd236ecc
Hunzilah16 <hunzilah16@gmail.com>
2024-03-24 21:57:56 +0500

    adding multipule functions to creat complexity

---
+++ Script lets start complicatibg thingssnippet #10.js
@@ -0,0 +1,20 @@
+function getMilk(money) {
+
+    var numberOfBottles =Math.floor(money/1.5);
+
+    console.log("buy" + numberOfBottles+ "botles of milk");
+
+    return money%1.5 //modulus gives the remainder f a division 
+
+    
+    
+}
+ var change = getMilk(9);
+// now if you concol log  change youll end up with a statmen which tells u how manny bottles u can buy and a integer which will tell u how much change u get left
+
+// dding some stuff
+
+function calculatingbotles( startingmoney , costperbottle ) {
+    var 
+    
+}
