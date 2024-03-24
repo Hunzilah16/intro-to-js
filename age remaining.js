41741c424865624841736a752938f6471302932e
Hunzilah16 <hunzilah16@gmail.com>
2024-03-24 20:57:39 +0500

    not working yet probably a syntex error

---
+++ age remaining.js
@@ -0,0 +1,14 @@
+function ageremainig(age) {
+
+    var remaing =age-90 ;
+
+    var years= remaing* 1;
+    var mounths= remaing* 12;
+    var weeks= remaing*54;
+    var days= remaing*365;
+    
+}
+
+ageremainig(20);
+
+console.log(" you have "+ years+ " years , " + mounths + " mounths , " + weeks + "weeks" + days+ " days Remaining . " );
