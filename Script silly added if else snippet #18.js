eefaeeef173ad66312679a3ace156f8583bcb810
Hunzilah16 <hunzilah16@gmail.com>
2024-03-26 00:20:13 +0500

    IF ELSE CONDITIONALS..... love calculator (somehing silly)

--- Script silly added if else snippet #18.js
+++ Script silly added if else snippet #18.js
@@ -5,7 +5,7 @@ var n= Math.random();
 n= n*100;
 n= Math.floor(n)+1;
 
-if (n>75) { alert("your love score is"+n+"you love them like kanye loves kanye ")
+if (n>75) { alert("your love score is"+n+"you both loves each other like kanye loves kanye ")
     
 } else { 
 alert("you got a " +n+"%"+ "chance")
