3beffe3e8da2ddb72e78bd46a31fc033a18b7020
Hunzilah16 <hunzilah16@gmail.com>
2024-03-26 00:18:09 +0500

    IF ELSE CONDITIONALS love calculator (somehing silly)

---
+++ Script silly added if else snippet #18.js
@@ -0,0 +1,13 @@
+prompt("your name");
+prompt("your crush's name");
+
+var n= Math.random();
+n= n*100;
+n= Math.floor(n)+1;
+
+if (n>75) { alert("your love score is"+n+"you love them like kanye loves kanye ")
+    
+} else { 
+alert("you got a " +n+"%"+ "chance")
+    
+} ;
