0ecf21f78b7bdaea1a1cca3078d6af9eafa1d61b
Hunzilah16 <hunzilah16@gmail.com>
2024-03-26 23:50:03 +0500

    intro to array

---
+++ Script INTRO TO ARRAY snippet #21.js
@@ -0,0 +1,9 @@
+var arry = ["a","b","c","d"];
+var check= prompt("whats your name?");
+
+if (check.includes("a","b","c","d")) {
+    alert("wellcome"+check);
+    
+} else {
+    alert("your not wellcom here");
+}
