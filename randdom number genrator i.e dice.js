236f47447850a52167f28b243bb7356f01b7d4f9
Hunzilah16 <hunzilah16@gmail.com>
2024-03-26 20:18:41 +0500

    randome number genrator i tried to add if else but failed

--- randdom number genrator i.e dice.js
+++ randdom number genrator i.e dice.js
@@ -4,3 +4,12 @@ n= Math.floor(n)+1;
 alert(n);
 
 //this gives a us a random number between 1 and 6//
+
+//adding if else
+
+// if (alert = true){
+//     alert(n);
+// }
+// else{
+//     alert(n);
+// }
