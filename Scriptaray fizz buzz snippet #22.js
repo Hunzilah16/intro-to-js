ec7b91b932999e4013067ddc1ffd485a369bf642
Hunzilah16 <hunzilah16@gmail.com>
2024-03-28 10:55:16 +0500

    fizzbuzz incomplet

---
+++ Scriptaray fizz buzz snippet #22.js
@@ -0,0 +1,6 @@
+//basic rles of fizz buzz
+
+//count from 1 to 100
+//but at any number divisiable by 3 we print fizz , and any number divisable by 5 we print buzz ,
+//and if the number is divisable by both we have to suout fizzbuzz.
+
