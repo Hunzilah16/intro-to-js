3f857a666efb71087741fc9bb8a6c4477dfd2947
Hunzilah16 <hunzilah16@gmail.com>
2024-03-31 15:24:57 +0500

    fizzbuzz kida solved still have some syntax errors but the logic is all aazz

---
+++ fizbuz solution.js
@@ -0,0 +1,49 @@
+// //basic rles of fizz buzz
+
+// //count from 1 to 100
+// //but at any number divisiable by 3 we print fizz , and any number divisable by 5 we print buzz ,
+// //and if the number is divisable by both we have to suout fizzbuzz.
+
+
+// var output =[];
+// var count = 1 ;
+
+// function numbers () {
+
+//     output.push(count);
+//     count++;
+    
+//     console.log(output);
+
+
+// }
+
+
+
+var outout = [];
+var count = 1;
+
+function increment () {
+    
+    output.push(count);
+    count=count+1;
+
+    if (count % 3===0) {
+  outout.push("fizz");       
+    }
+
+        else if (count%5===0) {
+            outout.push("buzz");
+        }
+            else if (count%5===0 && count%3===0) {
+                outout.push("fizzbuzz");
+            }
+    else { 
+        
+        console.log(outout);
+        
+    }
+    
+    console.log(output);
+    
+}
