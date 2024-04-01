dc45a61670b6314cd2be74c330a81d9cdc2b5dd4
Hunzilah16 <hunzilah16@gmail.com>
2024-04-01 11:07:16 +0500

    fizzbuzz automated doesnt work yet

---
+++ 99botels of beer.js
@@ -0,0 +1,21 @@
+// so lets say you didnt specify when yoy want the loop to end or the condition can never b false the 
+the loop will run forever and o called infinite loop and eventually the syestem will crash //
+
+//wanna crash a program ad a while loop with infaleable condition//
+
+//a programerss wife askes him while your at the store get sime milk he never cale back//
+
+var 99botelslyrics =[string];
+var string ( n+" bottles of beer on the wall, 7 bottles of beer."
+"Take one down and pass it around, "+n+"bottles of beer on the wall.");
+var n =(99);
+
+function 99botels() {
+    n=n-1;
+    while (n<100 && n>0) {
+        console.log 99botelslyrics;
+        
+    }
+
+ console.log 99botelslyrics;
+}
